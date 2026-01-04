8624546f7cc906da2eb81c5d0e14a25c976aba7f
specture258 <specture258@hanyang.ac.kr>
2026-01-04 16:36:17 +0900

    Feat: access토큰 재발급 api 추가

--- src/main/java/jaram/jaramplus/mopp_service/controller/AuthController.java
+++ src/main/java/jaram/jaramplus/mopp_service/controller/AuthController.java
@@ -1,7 +1,27 @@
 package jaram.jaramplus.mopp_service.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jaram.jaramplus.mopp_service.dto.TokenResponse;
+import jaram.jaramplus.mopp_service.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final MemberService memberService;
+
+
+    @PostMapping("/refresh")  //accessToken이 만료되었을 때 refreshToken을 통해 새로운 accesstoken생성
+    public ResponseEntity<TokenResponse> refreshToken(HttpServletRequest request, HttpServletResponse response){
+        TokenResponse tokenDto = memberService.validateToken(request, response);
+        return ResponseEntity.ok(tokenDto);
+    }
 }
--- src/main/java/jaram/jaramplus/mopp_service/service/AuthService.java
+++
@@ -1,7 +0,0 @@
-package jaram.jaramplus.mopp_service.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthService {
-}
